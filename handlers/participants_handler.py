203ee9aa7fe573a1b8f7107bf14cad001052f8bb
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-03-12 22:11:41 +0100

    log2debug

--- handlers/participants_handler.py
+++ handlers/participants_handler.py
@@ -15,6 +15,7 @@ class ParticipantsHandler:
             # no participant data yet
             for i in range(packet.num_active_cars):
                 _logger.info('New participant data created')
+                print(f'{i}:{packet.name}')
                 participant_data = packet.participants[i]
                 # TODO can the index here be correlated with the index in LapData ?
                 self.participants.append(Participant.create(participant_data))
