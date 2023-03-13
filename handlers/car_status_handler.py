21af024195593020e565b90a5b03cb8119503775
Xavier Dubuc <xavier.dubuc@bhc.be>
2023-03-13 19:44:32 +0100

    FIX

--- handlers/car_status_handler.py
+++ handlers/car_status_handler.py
@@ -19,8 +19,11 @@ class CarStatusHandler(AbstractHandler):
         else:
             for i, car_status_data in enumerate(packet.car_status_data):
                 car_status = drivers[i].get('car_status')
-                # an existing one is updated
-                if not car_status:
-                    drivers[i]['car_status'] = CarStatus.create(car_status_data)
+                if i < len(drivers):
+                    # an existing one is updated
+                    if not car_status:
+                        drivers[i]['car_status'] = CarStatus.create(car_status_data)
+                    else:
+                        drivers[i]['car_status'].update(car_status_data)
                 else:
-                    drivers[i]['car_status'].update(car_status_data)
+                    drivers[i].append({'car_status': CarStatus.create(car_status_data)})
